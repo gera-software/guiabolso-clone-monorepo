38e2c77df69d9219eed61d264d7a97e09039ffc8
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-25 10:36:46 -0300

    deleted src folder

--- apps/guiabolso-clone-server/src/api/getInstitutions.ts
+++
@@ -1,9 +0,0 @@
-import * as InstitutionRepository from '../repositories/institutionRepository'
-import { Institution } from "../types";
-
-
-export default async function () {
-    const institutions: Institution[] = await InstitutionRepository.fetchAll()
-
-    return institutions
-}
