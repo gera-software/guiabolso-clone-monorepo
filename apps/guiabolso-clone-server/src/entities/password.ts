81d9fe3b824a8e6aa31077d100eb72a2c9cf10a4
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-26 11:37:52 -0300

    feat [server]: password entity should accept valid password

--- apps/guiabolso-clone-server/src/entities/password.ts
+++ apps/guiabolso-clone-server/src/entities/password.ts
@@ -1,14 +1,18 @@
-import { left } from "@/shared"
+import { Either, left, right } from "@/shared"
 import { InvalidPasswordError } from "@/entities/errors"
 
 export class Password {
-    // public readonly value: string
+    public readonly value: string
 
     private constructor(value: string) {
-
+        this.value = value
     }
 
-    public static create(password: string) {
-        return left(new InvalidPasswordError("Password can't be empty"))
+    public static create(password: string): Either<InvalidPasswordError, Password> {
+        if(!password) {
+            return left(new InvalidPasswordError("Password can't be empty"))
+        }
+
+        return right(new Password(password))
     }
 }
--- apps/guiabolso-clone-server/test/entities/password.spec.ts
+++ apps/guiabolso-clone-server/test/entities/password.spec.ts
@@ -8,4 +8,10 @@ describe("Password value object", () => {
         expect(password).toBeInstanceOf(InvalidPasswordError)
         expect(password.message).toBe("Password can't be empty")
     })
+
+    test("should accept valid password", () => {
+        const validPassword = '123abc'
+        const password = Password.create(validPassword).value as Password
+        expect(password.value).toBe(validPassword)
+    })
 })
