7219aa7ec83250d23c84cf964525f0074faf4258
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-26 12:43:05 -0300

    feat [server]: sign up should not sign up user with invalid name

--- apps/guiabolso-clone-server/src/usecases/sign-up/sign-up.ts
+++ apps/guiabolso-clone-server/src/usecases/sign-up/sign-up.ts
@@ -1,14 +1,21 @@
 import { InMemoryUserRepository } from "@test/doubles"
 import { UserData } from "@/usecases/ports"
-import { InvalidEmailError } from "@/entities/errors"
-import { Either, left } from "@/shared"
+import { InvalidEmailError, InvalidNameError, InvalidPasswordError } from "@/entities/errors"
+import { Either, left, right } from "@/shared"
+import { User } from "@/entities"
 
 export class SignUp {
     constructor(userRepository: InMemoryUserRepository) {
 
     }
 
-    public async perform(request: UserData): Promise<Either<InvalidEmailError, UserData>> {
-        return left(new InvalidEmailError())
+    public async perform(request: UserData): Promise<Either<InvalidNameError | InvalidEmailError | InvalidPasswordError, UserData>> {
+        const userOrError = User.create(request)
+
+        if(userOrError.isLeft()) {
+            return left(userOrError.value)
+        }
+
+        return right(request)
     }
 }
--- apps/guiabolso-clone-server/test/usecases/sign-up.spec.ts
+++ apps/guiabolso-clone-server/test/usecases/sign-up.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidEmailError } from "@/entities/errors"
+import { InvalidEmailError, InvalidNameError, InvalidPasswordError } from "@/entities/errors"
 import { UserData } from "@/usecases/ports"
 import { SignUp } from "@/usecases/sign-up"
 import { InMemoryUserRepository } from "@test/doubles"
@@ -21,17 +21,45 @@ describe("Sing up use case", () => {
         expect((await emptyUserRepository.findByEmail(validUserSignUpRequest.email)).password).toEqual(validUserSignUpRequest.password)
     })
 
-    test("should not sign up user with invalid email", async () => {
-        const invalidUserSignUpRequest: UserData = {
-            name: 'any name',
-            email: 'invalidemail',
-            password: 'validpassword',
-        }
-
-        const emptyUserRepository = new InMemoryUserRepository([])
-        const sut: SignUp = new SignUp(emptyUserRepository)
-        const error = await sut.perform(invalidUserSignUpRequest)
-        expect(error.value).toBeInstanceOf(InvalidEmailError)
-
+    describe("should not sing up user with invalid data", () => {
+        test("should not sign up user with invalid email", async () => {
+            const invalidUserSignUpRequest: UserData = {
+                name: 'any name',
+                email: 'invalidemail',
+                password: 'validpassword',
+            }
+    
+            const emptyUserRepository = new InMemoryUserRepository([])
+            const sut: SignUp = new SignUp(emptyUserRepository)
+            const error = await sut.perform(invalidUserSignUpRequest)
+            expect(error.value).toBeInstanceOf(InvalidEmailError)
+        })
+    
+        test("should not sign up user with invalid password", async () => {
+            const invalidUserSignUpRequest: UserData = {
+                name: 'any name',
+                email: 'valid@email.com',
+                password: '',
+            }
+    
+            const emptyUserRepository = new InMemoryUserRepository([])
+            const sut: SignUp = new SignUp(emptyUserRepository)
+            const error = await sut.perform(invalidUserSignUpRequest)
+            expect(error.value).toBeInstanceOf(InvalidPasswordError)
+        })
+    
+        test("should not sign up user with invalid name", async () => {
+            const invalidUserSignUpRequest: UserData = {
+                name: '',
+                email: 'valid@email.com',
+                password: 'validpassword',
+            }
+    
+            const emptyUserRepository = new InMemoryUserRepository([])
+            const sut: SignUp = new SignUp(emptyUserRepository)
+            const error = await sut.perform(invalidUserSignUpRequest)
+            expect(error.value).toBeInstanceOf(InvalidNameError)
+        })
     })
+
 })
