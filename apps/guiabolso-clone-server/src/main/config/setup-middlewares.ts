dcdaeee92ec1093dcf2edd2e35856695799ec8c0
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-27 12:37:38 -0300

    feat [server]: disable cors middleware

--- apps/guiabolso-clone-server/src/main/config/setup-middlewares.ts
+++ apps/guiabolso-clone-server/src/main/config/setup-middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/middleware/body-parser'
 import { contentType } from '@/main/middleware/content-type'
+import { cors } from '@/main/middleware/cors'
 
 export default (app: Express): void => {
     app.use(bodyParser)
     app.use(contentType)
+    app.use(cors)
 }
