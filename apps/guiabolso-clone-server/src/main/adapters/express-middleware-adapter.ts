9690fc3415df1f5969c1c5397d2adc3b27cd15e4
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-27 12:46:29 -0300

    feat [server]: express middleware adapter

---
+++ apps/guiabolso-clone-server/src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,21 @@
+import { Middleware } from '@/web-controllers/middlewares/ports'
+import { Request, Response, NextFunction } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const request = {
+      accessToken: req.headers['x-access-token'],
+      // TODO o lugar de onde vem o user id não necessariamente será o body...
+      requesterId: req.body.userId
+    }
+    const httpResponse = await middleware.handle(request)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
