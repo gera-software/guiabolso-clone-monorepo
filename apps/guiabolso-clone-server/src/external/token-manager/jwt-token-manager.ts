d98f2e798674b9c3471493e0f8d285c40bf46c29
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-27 11:46:01 -0300

    feat [server]: jwt token manager adapter should correctly  verify if a json web token is expired

--- apps/guiabolso-clone-server/src/external/token-manager/jwt-token-manager.ts
+++ apps/guiabolso-clone-server/src/external/token-manager/jwt-token-manager.ts
@@ -1,8 +1,8 @@
-import { Either, right } from "@/shared";
+import { Either, left, right } from "@/shared";
 import { Payload, TokenManager } from "@/usecases/authentication/ports";
 import * as jwt from 'jsonwebtoken'
 
-export class JWtTokenManager implements TokenManager {
+export class JwtTokenManager implements TokenManager {
     private readonly secret: string
 
     constructor(secret: string) {
@@ -10,12 +10,19 @@ export class JWtTokenManager implements TokenManager {
     }
 
     async sign(info: Payload, expiresIn?: string | undefined): Promise<string> {
+        if(expiresIn) {
+            return jwt.sign(info, this.secret, { expiresIn })
+        }
         return jwt.sign(info, this.secret, { expiresIn: '30d' })
     }
 
     async verify(token: string): Promise<Either<Error, Payload>> {
-        const decoded = jwt.verify(token, this.secret) as Payload
-        return right(decoded)
+        try {
+            const decoded = jwt.verify(token, this.secret) as Payload
+            return right(decoded)
+        } catch(error) {
+            return left(error as Error)
+        }
     }
     
 }
