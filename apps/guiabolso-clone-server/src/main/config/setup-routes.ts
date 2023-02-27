88c4c0b89d8b37ef48320720eb3dc10ee59f88ee
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-27 16:39:21 -0300

    feat [server]: sign in route

--- apps/guiabolso-clone-server/src/main/config/setup-routes.ts
+++ apps/guiabolso-clone-server/src/main/config/setup-routes.ts
@@ -1,10 +1,13 @@
 import { Express, Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController } from '@/main/factories'
+import { makeSignInController, makeSignUpController } from '@/main/factories'
 
 export default (app: Express): void => {
     const router = Router()
     app.use('/api', router)
 
+    /** Cadastro */
     router.post('/signup', adaptRoute(makeSignUpController()))
+    /** Login */
+    router.post('/signin', adaptRoute(makeSignInController()))
 }
---
+++ apps/guiabolso-clone-server/test/main/routes/sign-in-route.test.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import { MongoHelper } from "@/external/repositories/mongodb/helper"
+import { makeEncoder, makeUserRepository } from "@/main/factories"
+import { UserData } from "@/usecases/ports"
+import app from '@/main/config/app'
+
+describe('Signin route', () => {
+
+    const validUser: UserData = {
+        name: "valid name",
+        email: "valid@email.com",
+        password: "valid"
+    }
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL ?? '')
+        await MongoHelper.clearCollection('users')
+        const userRepo = makeUserRepository()
+        const encoder = makeEncoder()
+
+        await userRepo.add({
+            name: validUser.name,
+            email: validUser.email,
+            password: await encoder.encode(validUser.password)
+        })
+    })
+
+    afterAll(async () => {
+        await MongoHelper.clearCollection('users')
+        await MongoHelper.disconnect()
+    })
+
+    test('should successfully sign in with valid user', async () => {
+        await request(app)
+            .post('/api/signin')
+            .send({
+                email: validUser.email,
+                password: validUser.password
+            })
+            .expect(200)
+            .then((res) => {
+                expect(res.body.accessToken).toBeDefined()
+                expect(res.body.id).toBeDefined()
+            })
+    })
+})
