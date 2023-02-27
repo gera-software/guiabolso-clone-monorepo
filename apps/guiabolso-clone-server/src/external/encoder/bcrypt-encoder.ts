e5f6f6e5e8452c6350bb97f8e618e554d3b19ebc
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-27 11:15:11 -0300

    feat [server]: bcrypt encoder adapter

---
+++ apps/guiabolso-clone-server/src/external/encoder/bcrypt-encoder.ts
@@ -0,0 +1,19 @@
+import { Encoder } from "@/usecases/ports";
+import * as bcrypt from 'bcrypt'
+
+export class BcryptEncoder implements Encoder {
+    private rounds: number
+
+    constructor(rounds: number) {
+        this.rounds = rounds
+    }
+
+    async encode(plain: string): Promise<string> {
+        return await bcrypt.hash(plain, this.rounds)
+    }
+
+    async compare(plain: string, hashed: string): Promise<boolean> {
+        return await bcrypt.compare(plain, hashed)
+    }
+
+}
