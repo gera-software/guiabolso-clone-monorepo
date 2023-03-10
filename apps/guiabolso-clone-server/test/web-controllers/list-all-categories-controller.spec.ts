ae32f18ffc598b3d21e731ba5cb2aa74fe686493
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-10 17:22:51 -0300

    feat [server]: list all categories controller

---
+++ apps/guiabolso-clone-server/test/web-controllers/list-all-categories-controller.spec.ts
@@ -0,0 +1,48 @@
+import { ListAllCategories } from "@/usecases/list-all-categories"
+import { CategoryData, UseCase } from "@/usecases/ports"
+import { ListAllCategoriesController } from "@/web-controllers"
+import { HttpRequest, HttpResponse } from "@/web-controllers/ports"
+import { InMemoryCategoryRepository } from "@test/doubles/repositories"
+import { ErrorThrowingUseCaseStub } from "@test/doubles/usecases"
+
+const categoriesArray: CategoryData[] = []
+
+categoriesArray.push({
+    name: "category 0",
+    group: "group 0",
+    iconName: "icon 0",
+    primaryColor: "color 0",
+    id: "0",
+})
+categoriesArray.push({
+    name: "category 1",
+    group: "group 1",
+    iconName: "icon 1",
+    primaryColor: "color 1",
+    id: "1",
+})
+
+
+describe('List all categories web controller', () => {
+    test('should return status code 200', async () => {
+        const categoryRepo = new InMemoryCategoryRepository(categoriesArray)
+        const usecase = new ListAllCategories(categoryRepo)
+        const sut = new ListAllCategoriesController(usecase)
+
+        const validRequest: HttpRequest = {}
+
+        const response: HttpResponse = await sut.handle(validRequest)
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.length).toEqual(2)
+    })
+
+    test('should return status code 500 when server raises', async () => {
+        const errorThrowingUseCaseStub: UseCase = new ErrorThrowingUseCaseStub()
+        const sut = new ListAllCategoriesController(errorThrowingUseCaseStub)
+
+        const validRequest: HttpRequest = {}
+
+        const response: HttpResponse = await sut.handle(validRequest)
+        expect(response.statusCode).toEqual(500)
+    })
+})
