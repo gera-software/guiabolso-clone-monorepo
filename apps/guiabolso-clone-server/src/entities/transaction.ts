f4c50f4ac631d0d3afa910db92d382f9d30d01be
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-11 17:38:22 -0300

    feat [server]: should not create transaction without description or descriptionOriginal

--- apps/guiabolso-clone-server/src/entities/transaction.ts
+++ apps/guiabolso-clone-server/src/entities/transaction.ts
@@ -1,23 +1,51 @@
 import { TransactionData } from "@/usecases/ports"
 import { InvalidTransactionError } from "@/entities/errors"
 import { left, right } from "@/shared"
-import { Account, Category, User } from "@/entities"
+import { Account, Amount, Category, User } from "@/entities"
 
 export class Transaction {
     public readonly account: Account
     public readonly category: Category
+    public readonly amount: Amount
+    public readonly description: string
+    public readonly descriptionOriginal: string
 
-    private constructor(transaction: { account: Account, category: Category }) {
+    private constructor(transaction: { account: Account, category: Category, amount: Amount, description?: string, descriptionOriginal?: string }) {
         this.account = transaction.account
+        this.amount = transaction.amount
         this.category = transaction.category ?? null
+        this.description = transaction.description ?? ''
+        this.descriptionOriginal = transaction.descriptionOriginal ?? ''
     }
 
-    public static create(transaction: { account: Account, category?: Category }) {
+    public static create(transaction: { account: Account, category?: Category, amount: number, description?: string, descriptionOriginal?: string }) {
 
         if(!transaction.account) {
             return left(new InvalidTransactionError('Invalid account'))
         }
 
-        return right(new Transaction({ account: transaction.account, category: transaction.category }))
+        if(!transaction.description && !transaction.descriptionOriginal) {
+            return left(new InvalidTransactionError('Required some description'))
+        }
+
+        if(transaction.amount === 0) {
+            return left(new InvalidTransactionError('Invalid amount'))
+        }
+
+        const amountOrError = Amount.create(transaction.amount)
+
+        if(amountOrError.isLeft()) {
+            return left(amountOrError.value)
+        }
+
+        const amount = amountOrError.value as Amount
+
+        return right(new Transaction({ 
+            account: transaction.account, 
+            amount, 
+            category: transaction.category,
+            description: transaction.description,
+            descriptionOriginal: transaction.descriptionOriginal,
+        }))
     }
 }
--- apps/guiabolso-clone-server/test/entities/transaction.spec.ts
+++ apps/guiabolso-clone-server/test/entities/transaction.spec.ts
@@ -7,7 +7,7 @@ describe("Transaction entity", () => {
     const imageUrl = 'valid image url'
     const user = User.create({
         name: 'user name',
-        email: 'user@email',
+        email: 'user@email.com',
         password: 'user password',
     }).value as User
     const walletAccount = WalletAccount.create({name, balance, imageUrl, user}).value as WalletAccount
@@ -21,21 +21,58 @@ describe("Transaction entity", () => {
     }).value as Category
 
     test("should not create transaction with empty account", () => {
-        const error = Transaction.create({ account: null }).value as Error
+        const amount = 1900
+        const description = 'transaction description'
+        const error = Transaction.create({ account: null, amount, description }).value as Error
         expect(error).toBeInstanceOf(InvalidTransactionError)
         expect(error.message).toBe('Invalid account')
     })
 
+    test("should not create transaction with zero amount", () => {
+        const description = 'transaction description'
+        const error = Transaction.create({ account: walletAccount, amount: 0, description }).value as Error
+        expect(error).toBeInstanceOf(InvalidTransactionError)
+        expect(error.message).toBe('Invalid amount')
+    })
+
     test("should create transaction with valid account", () => {
-        const transaction = Transaction.create({ account: walletAccount }).value as Transaction
+        const amount = 1900
+        const description = 'transaction description'
+        const transaction = Transaction.create({ account: walletAccount, amount, description }).value as Transaction
         expect(transaction.account).toEqual(walletAccount)
         expect(transaction.category).toEqual(null)
+        expect(transaction.amount.value).toEqual(amount)
+        expect(transaction.description).toEqual(description)
+        expect(transaction.descriptionOriginal).toEqual('')
     })
-    
+
     test("should create transaction with optional category", () => {
-        const transaction = Transaction.create({ account: walletAccount, category }).value as Transaction
+        const amount = 1900
+        const description = 'transaction description'
+        const transaction = Transaction.create({ account: walletAccount, amount, category, description }).value as Transaction
         expect(transaction.account).toEqual(walletAccount)
         expect(transaction.category).toEqual(category)
+        expect(transaction.amount.value).toEqual(amount)
+        expect(transaction.description).toEqual(description)
+        expect(transaction.descriptionOriginal).toEqual('')
+    })
+
+    test("should not create transaction without description or descriptionOriginal", () => {
+        const amount = 1900
+        const error = Transaction.create({ account: walletAccount, amount, category }).value as Error
+        expect(error).toBeInstanceOf(InvalidTransactionError)
+        expect(error.message).toBe('Required some description')
+    })
+
+    test("should create transaction with descriptionOriginal", () => {
+        const amount = 1900
+        const description = 'transaction description'
+        const transaction = Transaction.create({ account: walletAccount, amount, descriptionOriginal: description }).value as Transaction
+        expect(transaction.account).toEqual(walletAccount)
+        expect(transaction.category).toEqual(null)
+        expect(transaction.amount.value).toEqual(amount)
+        expect(transaction.descriptionOriginal).toEqual(description)
+        expect(transaction.description).toEqual('')
     })
 
 })
