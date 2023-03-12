5545255752b91e360a7900eb42b0c4460d6d78e8
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-12 12:21:02 -0300

    feat [server]: add method 'add()' to amount entity

--- apps/guiabolso-clone-server/src/entities/amount.ts
+++ apps/guiabolso-clone-server/src/entities/amount.ts
@@ -1,12 +1,16 @@
 import { Either, left, right } from "@/shared";
-import { InvalidAmountError } from "./errors";
+import { InvalidAmountError } from "@/entities/errors";
 
 export class Amount {
-    public readonly value: number
+    private _value: number
 
     private constructor(value: number) {
-        this.value = value
+        this._value = value
     }
+
+    public get value() {
+        return this._value
+    } 
 
     public static create(value: number): Either<InvalidAmountError, Amount> {
         if(!Number.isInteger(value)) {
@@ -14,5 +18,9 @@ export class Amount {
         }
 
         return right(new Amount(value))
+    }
+
+    public add(value: number) {
+        this._value += value
     }
 }
--- apps/guiabolso-clone-server/test/entities/amount.spec.ts
+++ apps/guiabolso-clone-server/test/entities/amount.spec.ts
@@ -2,27 +2,47 @@ import { Amount } from "@/entities"
 import { InvalidAmountError } from "@/entities/errors"
 
 describe("Amount value object", () => {
-    test("should not create an amount if it isn't an integer value", () => {
-        const invalidAmount = 5.69
-        const error = Amount.create(invalidAmount).value as Error
-        expect(error).toBeInstanceOf(InvalidAmountError)
+    describe("create", () => {
+        test("should not create an amount if it isn't an integer value", () => {
+            const invalidAmount = 5.69
+            const error = Amount.create(invalidAmount).value as Error
+            expect(error).toBeInstanceOf(InvalidAmountError)
+        })
+    
+        test("should create an amount if it is a positive integer value", () => {
+            const validAmount = 569
+            const amount = Amount.create(validAmount).value as Amount
+            expect(amount.value).toBe(validAmount)
+        })
+    
+        test("should create an amount if it is a negative integer value", () => {
+            const validAmount = -569
+            const amount = Amount.create(validAmount).value as Amount
+            expect(amount.value).toBe(validAmount)
+        })
+    
+        test("should create an amount if it is zero value", () => {
+            const validAmount = 0
+            const amount = Amount.create(validAmount).value as Amount
+            expect(amount.value).toBe(validAmount)
+        })
     })
 
-    test("should create an amount if it is a positive integer value", () => {
-        const validAmount = 569
-        const amount = Amount.create(validAmount).value as Amount
-        expect(amount.value).toBe(validAmount)
-    })
+    describe('add', () => {
+        test("should add a positive value to the amount", () => {
+            const valueToAdd = 123
+            const validAmount = 50
+            const amount = Amount.create(validAmount).value as Amount
+            amount.add(valueToAdd)
+            expect(amount.value).toBe(validAmount + valueToAdd)
+        })
 
-    test("should create an amount if it is a negative integer value", () => {
-        const validAmount = -569
-        const amount = Amount.create(validAmount).value as Amount
-        expect(amount.value).toBe(validAmount)
-    })
-
-    test("should create an amount if it is zero value", () => {
-        const validAmount = 0
-        const amount = Amount.create(validAmount).value as Amount
-        expect(amount.value).toBe(validAmount)
+        test("should add a negative value to the amount", () => {
+            const valueToAdd = -123
+            const validAmount = 50
+            const amount = Amount.create(validAmount).value as Amount
+            amount.add(valueToAdd)
+            expect(amount.value).toBe(validAmount + valueToAdd)
+        })
     })
 })
