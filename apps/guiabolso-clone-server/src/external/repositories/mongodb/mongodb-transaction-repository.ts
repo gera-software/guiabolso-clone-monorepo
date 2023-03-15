7d146535ac59271bf6b7f18006bd01a76ec41e20
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-15 19:21:51 -0300

    feat [server]: MongodbTransactionRepository basic operations

---
+++ apps/guiabolso-clone-server/src/external/repositories/mongodb/mongodb-transaction-repository.ts
@@ -0,0 +1,117 @@
+import { CategoryData, TransactionData, TransactionRepository } from "@/usecases/ports";
+import { ObjectId } from "mongodb";
+import { MongodbCategory } from "@/external/repositories/mongodb";
+import { MongoHelper } from "@/external/repositories/mongodb/helper";
+
+export type MongodbTransaction = {
+    _id?: ObjectId,
+    accountId: ObjectId,
+    accountType: string,
+    syncType: string,
+    userId: ObjectId,
+    amount: number,
+    description?: string,
+    descriptionOriginal?: string,
+    date: Date,
+    type: string,
+    comment?: string,
+    ignored?: boolean,
+    category?: MongodbCategory,
+    _isDeleted?: boolean,
+}
+
+export class MongodbTransactionRepository implements TransactionRepository {
+    async add(transaction: TransactionData): Promise<TransactionData> {
+        const transactionCollection = MongoHelper.getCollection('transactions')
+
+        const transactionClone: MongodbTransaction = {
+            accountId: new ObjectId(transaction.accountId),
+            accountType: transaction.accountType,
+            syncType: transaction.syncType,
+            userId: new ObjectId(transaction.userId),
+            amount: transaction.amount,
+            description: transaction.description,
+            descriptionOriginal: transaction.descriptionOriginal,
+            date: transaction.date,
+            type: transaction.type,
+            comment: transaction.comment,
+            ignored: transaction.ignored,
+            _isDeleted: transaction._isDeleted,
+        }
+
+        if(transaction.category) {
+            const categoryClone: MongodbCategory = {
+                _id: new ObjectId(transaction.category.id),
+                name: transaction.category.name,
+                group: transaction.category.group,
+                iconName: transaction.category.iconName,
+                primaryColor: transaction.category.primaryColor,
+                ignored: transaction.category.ignored
+            }
+
+            transactionClone.category = categoryClone
+        }
+
+        const { insertedId } = await transactionCollection.insertOne(transactionClone)
+
+        return this.findById(insertedId.toString())
+    }
+
+    async findById(id: string): Promise<TransactionData> {
+        const transactionCollection = MongoHelper.getCollection('transactions')
+        const transaction = await transactionCollection.findOne<MongodbTransaction>({ _id: new ObjectId(id) })
+        if(transaction) {
+            return this.withApplicationId(transaction)
+        }
+        return null
+    }
+
+    async exists(id: string): Promise<boolean> {
+        const result = await this.findById(id)
+        if(result != null) {
+            return true
+        }
+        return false
+    }
+
+    async remove(id: string): Promise<TransactionData> {
+        throw new Error("Method not implemented.");
+    }
+
+    async update(transaction: TransactionData): Promise<TransactionData> {
+        throw new Error("Method not implemented.");
+    }
+
+    private withApplicationId (dbTransaction: MongodbTransaction): TransactionData {
+        let category: CategoryData = null
+
+        if(dbTransaction.category) {
+            category = {
+                id: dbTransaction.category._id.toString(),
+                name: dbTransaction.category.name,
+                group: dbTransaction.category.group,
+                iconName: dbTransaction.category.iconName,
+                primaryColor: dbTransaction.category.primaryColor,
+                ignored: dbTransaction.category.ignored
+            }
+        }
+
+        return {
+            id: dbTransaction._id.toString(),
+            accountId: dbTransaction.accountId.toString(),
+            accountType: dbTransaction.accountType,
+            syncType: dbTransaction.syncType,
+            userId: dbTransaction.userId.toString(),
+            amount: dbTransaction.amount,
+            description: dbTransaction.description,
+            descriptionOriginal: dbTransaction.descriptionOriginal,
+            date: dbTransaction.date,
+            type: dbTransaction.type,
+            comment: dbTransaction.comment,
+            ignored: dbTransaction.ignored,
+            _isDeleted: dbTransaction._isDeleted,
+            category,
+        }
+    }
+    
+}
---
+++ apps/guiabolso-clone-server/test/external/repositories/mongodb/mongodb-transaction-repository.spec.ts
@@ -0,0 +1,157 @@
+import { MongodbAccountRepository, MongodbCategory, MongodbCategoryRepository, MongodbTransactionRepository, MongodbUserRepository } from "@/external/repositories/mongodb"
+import { MongoHelper } from "@/external/repositories/mongodb/helper"
+import { CategoryData, TransactionData, UserData, WalletAccountData } from "@/usecases/ports"
+
+describe('Mongodb Transaction repository', () => { 
+    const userRepo = new MongodbUserRepository()
+    let validUser: UserData
+
+    const accountRepo = new MongodbAccountRepository()
+    let validWalletAccount: WalletAccountData
+
+    const categoryRepo = new MongodbCategoryRepository()
+    let validCategory0: CategoryData
+    let validCategory1: CategoryData
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+        const user = {
+            name: 'any_name',
+            email: 'any@mail.com',
+            password: '123',
+        }
+        validUser = await userRepo.add(user)
+
+        const walletAccount: WalletAccountData = {
+            type: "WALLET",
+            syncType: "MANUAL",
+            name: "valid wallet account",
+            balance: 0,
+            userId: validUser.id,
+        }
+        validWalletAccount = await accountRepo.add(walletAccount)
+
+        const category0: MongodbCategory = {
+            _id: null,
+            name: "category 0",
+            group: "VALIDGROUP",
+            iconName: "ICON 0",
+            primaryColor: "COLOR 0",
+            ignored: false
+        }
+        const category1: MongodbCategory = {
+            _id: null,
+            name: "category 1",
+            group: "VALIDGROUP",
+            iconName: "ICON 1",
+            primaryColor: "COLOR 1",
+            ignored: false
+        }
+
+        const categoryCollection = MongoHelper.getCollection('categories')
+        await categoryCollection.insertMany([ category0, category1 ])
+
+        const [ cat0, cat1 ]= await categoryRepo.fetchAll()
+        validCategory0 = cat0
+        validCategory1 = cat1
+    })
+
+    afterAll(async () => {
+        await MongoHelper.clearCollection('users')
+        await MongoHelper.clearCollection('accounts')
+        await MongoHelper.clearCollection('categories')
+        await MongoHelper.clearCollection('transactions')
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        await MongoHelper.clearCollection('users')
+        await MongoHelper.clearCollection('accounts')
+        await MongoHelper.clearCollection('categories')
+        await MongoHelper.clearCollection('transactions')
+    })
+
+    test('when transaction is added, it should exist', async () => {
+        const sut = new MongodbTransactionRepository()
+        const transactionData: TransactionData = {
+            accountId: validWalletAccount.id,
+            accountType: 'WALLET',
+            syncType: 'MANUAL',
+            userId: validUser.id,
+            amount: 2345,
+            description: 'valid description',
+            descriptionOriginal: '',
+            date: new Date('2023-05-18'),
+            type: 'INCOME',
+            comment: 'valid comment',
+            ignored: false,
+            category: {
+                id: validCategory0.id,
+                name: "category 0",
+                group: "group 0",
+                iconName: "icon 0",
+                primaryColor: "color 0",
+                ignored: true,
+            },
+            _isDeleted: false,
+        }
+
+        const addedTransaction =  await sut.add(transactionData)
+        expect(addedTransaction.id).toBeDefined()
+        const exists = await sut.exists(addedTransaction.id)
+        expect(exists).toBeTruthy()
+    })
+
+    test('when a transaction is not find by id, should return null', async () => {
+        const notFoundId = '62f95f4a93d61d8fff971668'
+        const sut = new MongodbTransactionRepository()
+        const result = await sut.findById(notFoundId)
+        expect(result).toBeNull()
+    })
+
+    test('when a transaction is find by id, should return the transaction', async () => {
+        const sut = new MongodbTransactionRepository()
+        const transactionData: TransactionData = {
+            accountId: validWalletAccount.id,
+            accountType: 'WALLET',
+            syncType: 'MANUAL',
+            userId: validUser.id,
+            amount: 2345,
+            description: 'valid description',
+            descriptionOriginal: '',
+            date: new Date('2023-05-18'),
+            type: 'INCOME',
+            comment: 'valid comment',
+            ignored: false,
+            category: {
+                id: validCategory0.id,
+                name: "category 0",
+                group: "group 0",
+                iconName: "icon 0",
+                primaryColor: "color 0",
+                ignored: true,
+            },
+            _isDeleted: false,
+        }
+
+        const addedTransaction =  await sut.add(transactionData)
+
+        const result: TransactionData = await sut.findById(addedTransaction.id) as TransactionData
+        expect(result).not.toBeNull()
+        expect(result.id).toBe(addedTransaction.id)
+        expect(result.accountId).toBe(transactionData.accountId)
+        expect(result.accountType).toBe(transactionData.accountType)
+        expect(result.syncType).toBe(transactionData.syncType)
+        expect(result.userId).toBe(transactionData.userId)
+        expect(result.amount).toBe(transactionData.amount)
+        expect(result.description).toBe(transactionData.description)
+        expect(result.descriptionOriginal).toBe(transactionData.descriptionOriginal)
+        expect(result.date).toEqual(transactionData.date)
+        expect(result.type).toBe(transactionData.type)
+        expect(result.comment).toBe(transactionData.comment)
+        expect(result.ignored).toBe(transactionData.ignored)
+        expect(result.category).toEqual(transactionData.category)
+        expect(result._isDeleted).toBe(transactionData._isDeleted)
+
+    })
+})
