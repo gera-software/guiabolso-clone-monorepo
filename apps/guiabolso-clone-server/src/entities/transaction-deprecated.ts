c12046de900a3b7b7d488e6e3935198b0756b304
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-18 18:55:38 -0300

    refactor [server]: created Transaction interface

--- apps/guiabolso-clone-server/src/entities/transaction-deprecated.ts
+++ apps/guiabolso-clone-server/src/entities/transaction-deprecated.ts
@@ -1,29 +1,31 @@
 import { InvalidAmountError, InvalidTransactionError } from "@/entities/errors"
 import { Either, left, right } from "@/shared"
-import { Amount, Category, Transaction, TransactionType } from "@/entities"
+import { Amount, Category, TransactionType } from "@/entities"
 
-export class WalletTransaction implements Transaction {
+export class TransactionDeprecated {
     public readonly category: Category
     public readonly amount: Amount
     public readonly description: string
+    public readonly descriptionOriginal: string // TODO this field doesn't belong to wallet transactions
     public readonly date: Date
     public readonly comment: string
     public readonly ignored: boolean
     public readonly type: TransactionType
 
-    private constructor(transaction: { category: Category, amount: Amount, description?: string, date: Date, comment?: string, ignored?: boolean }) {
+    private constructor(transaction: { category: Category, amount: Amount, description?: string, descriptionOriginal?: string, date: Date, comment?: string, ignored?: boolean }) {
         this.amount = transaction.amount
         this.category = transaction.category ?? null
         this.description = transaction.description ?? ''
+        this.descriptionOriginal = transaction.descriptionOriginal ?? ''
         this.date = transaction.date
         this.comment = transaction.comment ?? ''
         this.ignored = !!transaction.ignored
         this.type = (this.amount.value >= 0) ? 'INCOME' : 'EXPENSE'
     }
 
-    public static create(transaction: { category?: Category, amount: number, description?: string, date: Date, comment?: string, ignored?: boolean }): Either<InvalidTransactionError | InvalidAmountError, WalletTransaction> {
+    public static create(transaction: { category?: Category, amount: number, description?: string, descriptionOriginal?: string, date: Date, comment?: string, ignored?: boolean }): Either<InvalidTransactionError | InvalidAmountError, TransactionDeprecated> {
 
-        if(!transaction.description) {
+        if(!transaction.description && !transaction.descriptionOriginal) {
             return left(new InvalidTransactionError('Required some description'))
         }
 
@@ -39,10 +41,11 @@ export class WalletTransaction implements Transaction {
 
         const amount = amountOrError.value as Amount
 
-        return right(new WalletTransaction({ 
+        return right(new TransactionDeprecated({ 
             amount, 
             category: transaction.category,
             description: transaction.description,
+            descriptionOriginal: transaction.descriptionOriginal,
             date: transaction.date,
             comment: transaction.comment,
             ignored: transaction.ignored,
--- apps/guiabolso-clone-server/src/entities/transaction.ts
+++ apps/guiabolso-clone-server/src/entities/transaction.ts
@@ -1,56 +1,13 @@
-import { InvalidAmountError, InvalidTransactionError } from "@/entities/errors"
-import { Either, left, right } from "@/shared"
 import { Amount, Category } from "@/entities"
 
 export type TransactionType = 'EXPENSE' | 'INCOME'
 
-export class TransactionDeprecated {
-    public readonly category: Category
-    public readonly amount: Amount
-    public readonly description: string
-    public readonly descriptionOriginal: string // TODO this field doesn't belong to wallet transactions
-    public readonly date: Date
-    public readonly comment: string
-    public readonly ignored: boolean
-    public readonly type: TransactionType
-
-    private constructor(transaction: { category: Category, amount: Amount, description?: string, descriptionOriginal?: string, date: Date, comment?: string, ignored?: boolean }) {
-        this.amount = transaction.amount
-        this.category = transaction.category ?? null
-        this.description = transaction.description ?? ''
-        this.descriptionOriginal = transaction.descriptionOriginal ?? ''
-        this.date = transaction.date
-        this.comment = transaction.comment ?? ''
-        this.ignored = !!transaction.ignored
-        this.type = (this.amount.value >= 0) ? 'INCOME' : 'EXPENSE'
-    }
-
-    public static create(transaction: { category?: Category, amount: number, description?: string, descriptionOriginal?: string, date: Date, comment?: string, ignored?: boolean }): Either<InvalidTransactionError | InvalidAmountError, TransactionDeprecated> {
-
-        if(!transaction.description && !transaction.descriptionOriginal) {
-            return left(new InvalidTransactionError('Required some description'))
-        }
-
-        if(transaction.amount === 0) {
-            return left(new InvalidTransactionError('Invalid amount'))
-        }
-
-        const amountOrError = Amount.create(transaction.amount)
-
-        if(amountOrError.isLeft()) {
-            return left(amountOrError.value)
-        }
-
-        const amount = amountOrError.value as Amount
-
-        return right(new TransactionDeprecated({ 
-            amount, 
-            category: transaction.category,
-            description: transaction.description,
-            descriptionOriginal: transaction.descriptionOriginal,
-            date: transaction.date,
-            comment: transaction.comment,
-            ignored: transaction.ignored,
-        }))
-    }
+export interface Transaction {
+    category: Category
+    amount: Amount
+    description: string
+    date: Date
+    comment: string
+    ignored: boolean
+    type: TransactionType
 }
