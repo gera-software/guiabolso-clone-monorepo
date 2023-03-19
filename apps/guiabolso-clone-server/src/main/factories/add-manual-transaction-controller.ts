664a8f5721f40334da6a15e085ec4e0f404f6e08
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-19 15:00:10 -0300

    fix [server]: makeAddManualTransactionController suport bank transactions

--- apps/guiabolso-clone-server/src/main/factories/add-manual-transaction-controller.ts
+++ apps/guiabolso-clone-server/src/main/factories/add-manual-transaction-controller.ts
@@ -3,15 +3,17 @@ import { AddManualTransactionToWallet } from "@/usecases/add-manual-transaction-
 import { AddManualTransactionController } from "@/web-controllers";
 import { Controller } from "@/web-controllers/ports";
 import { makeAccountRepository, makeCategoryRepository, makeTransactionRepository, makeUserRepository } from "@/main/factories";
+import { AddManualTransactionToBank } from "@/usecases/add-manual-transaction-to-bank";
 
 export const makeAddManualTransactionController = (): Controller => {
     const userRepository = makeUserRepository()
     const accountRepository = makeAccountRepository()
     const categoryRepository = makeCategoryRepository()
     const transactionRepository = makeTransactionRepository()
-    const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+    const addManualTransactionToWallet = new AddManualTransactionToWallet(accountRepository, transactionRepository)
+    const addManualTransactionToBank = new AddManualTransactionToBank(accountRepository, transactionRepository)
     
-    const usecase = new AddManualTransaction(addManualTransactionToWallet)
+    const usecase = new AddManualTransaction(userRepository, accountRepository, categoryRepository, addManualTransactionToWallet, addManualTransactionToBank)
     const controller = new AddManualTransactionController(usecase)
     return controller
 }
