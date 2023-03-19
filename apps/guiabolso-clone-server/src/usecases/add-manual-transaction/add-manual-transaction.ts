282af51d89c61df5c526662f382800b4e633e63a
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-19 11:43:44 -0300

    refactor [server]: add manual transaction use case should check if any required entity is unregistered

--- apps/guiabolso-clone-server/src/usecases/add-manual-transaction/add-manual-transaction.ts
+++ apps/guiabolso-clone-server/src/usecases/add-manual-transaction/add-manual-transaction.ts
@@ -1,18 +1,49 @@
-import { TransactionData, TransactionRequest, UseCase } from "@/usecases/ports";
+import { CategoryData, CategoryRepository, TransactionData, TransactionRequest, UpdateAccountRepository, UseCase, UserRepository } from "@/usecases/ports";
 import { AddManualTransactionToWallet } from "@/usecases/add-manual-transaction-to-wallet";
 import { InvalidAmountError, InvalidBalanceError, InvalidEmailError, InvalidNameError, InvalidPasswordError, InvalidTransactionError } from "@/entities/errors";
-import { Either } from "@/shared";
+import { Either, left } from "@/shared";
 import { UnregisteredAccountError, UnregisteredUserError, UnregisteredCategoryError } from "@/usecases/errors"
+import { Category, User } from "@/entities";
 
 export class AddManualTransaction implements UseCase {
-    private addManualTransactionToWallet: AddManualTransactionToWallet
+    private readonly accountRepo: UpdateAccountRepository
+    private readonly userRepo: UserRepository
+    private readonly categoryRepo: CategoryRepository
+    
 
-    constructor(addManualTransactionToWallet: AddManualTransactionToWallet) {
+    private addManualTransactionToWallet: UseCase
+    private addManualTransactionToBank: UseCase
+
+    constructor(userRepository: UserRepository, accountRepository: UpdateAccountRepository, categoryRepository: CategoryRepository, addManualTransactionToWallet: UseCase, addManualTransactionToBank: UseCase, ) {
+        this.userRepo = userRepository
+        this.accountRepo = accountRepository
+        this.categoryRepo = categoryRepository
+
+        
         this.addManualTransactionToWallet = addManualTransactionToWallet
+        this.addManualTransactionToBank = addManualTransactionToBank
     }
 
-    async perform(request: TransactionRequest): Promise<Either<UnregisteredAccountError | UnregisteredUserError | InvalidNameError | InvalidEmailError | InvalidPasswordError | InvalidBalanceError | UnregisteredCategoryError | InvalidTransactionError | InvalidAmountError, TransactionData>> {
-        return this.addManualTransactionToWallet.perform(request)
+    async perform(request: TransactionRequest): Promise<any> {
+        const foundAccountData = await this.accountRepo.findById(request.accountId)
+        if(!foundAccountData) {
+            return left(new UnregisteredAccountError())
+        }
+
+        const foundUserData = await this.userRepo.findUserById(foundAccountData.userId)
+        if(!foundUserData) {
+            return left(new UnregisteredUserError())
+        }
+
+        let foundCategoryData: CategoryData = null
+        // if(request.categoryId) {
+            foundCategoryData = await this.categoryRepo.findById(request.categoryId)
+            if(!foundCategoryData) {
+                return left(new UnregisteredCategoryError())
+            }
+        // }
+
+        // return this.addManualTransactionToWallet.perform(request)
     }
 
 }
--- apps/guiabolso-clone-server/test/usecases/add-manual-transaction.spec.ts
+++ apps/guiabolso-clone-server/test/usecases/add-manual-transaction.spec.ts
@@ -1,74 +1,67 @@
 import { InvalidTransactionError } from "@/entities/errors"
 import { AddManualTransaction } from "@/usecases/add-manual-transaction"
+import { AddManualTransactionToBank } from "@/usecases/add-manual-transaction-to-bank"
 import { AddManualTransactionToWallet } from "@/usecases/add-manual-transaction-to-wallet"
-import { CategoryData, TransactionData, TransactionRequest, WalletAccountData } from "@/usecases/ports"
+import { UnregisteredAccountError, UnregisteredCategoryError, UnregisteredUserError } from "@/usecases/errors"
+import { BankAccountData, CategoryData, TransactionData, TransactionRequest, WalletAccountData } from "@/usecases/ports"
 import { InMemoryAccountRepository, InMemoryCategoryRepository, InMemoryTransactionRepository, InMemoryUserRepository } from "@test/doubles/repositories"
 
 describe('Add manual transaction to account use case', () => {
-    const userId = 'u0'
-    const categoryId = 'c0'
-    const amount = -5060
-    const description = 'valid description'
-    const date = new Date('2023-03-09')
-    const comment = 'valid comment'
-    const ignored = false
 
-    const categoryData: CategoryData = {
-        name: "category 0",
-        group: "group 0",
-        iconName: "icon 0",
-        primaryColor: "color 0",
-        ignored: true,
-        id: categoryId,
-    }
+    describe('verifications', () => {
+        const userId = 'u0'
+        const categoryId = 'c0'
+        const amount = -5060
+        const description = 'valid description'
+        const date = new Date('2023-03-09')
+        const comment = 'valid comment'
+        const ignored = false
     
-    
-    const accountId = 'ac0'
-    const accountType = 'WALLET'
-    const syncType = 'MANUAL'
-    const name = 'valid account'
-    const balance = 678
-    const imageUrl = 'valid image url'
-
-    let walletAccountData: WalletAccountData
-
-    beforeEach(() => {
-        walletAccountData = {
-            id: accountId,
-            type: accountType,
-            syncType,
-            name,
-            balance,
-            imageUrl,
-            userId,
+        const categoryData: CategoryData = {
+            name: "category 0",
+            group: "group 0",
+            iconName: "icon 0",
+            primaryColor: "color 0",
+            ignored: true,
+            id: categoryId,
         }
-    })
-    describe('add manual transaction to wallet account', () => {
-        test('should not add transaction without required field', async () => {
-            const transactionRequest: TransactionRequest = {
-                accountId,
-                categoryId,
-                amount,
-                date,
-                comment,
-                ignored,
+        
+        
+        const walletAccountId = 'wac0'
+        const walletAccountType = 'WALLET'
+        const bankAccountId = 'bac0'
+        const bankAccountType = 'BANK'
+        const syncType = 'MANUAL'
+        const balance = 678
+        const imageUrl = 'valid image url'
+    
+        let walletAccountData: WalletAccountData
+        let bankAccountData: BankAccountData
+    
+        beforeEach(() => {
+            walletAccountData = {
+                id: walletAccountId,
+                type: walletAccountType,
+                syncType,
+                name: 'valid wallet account',
+                balance,
+                imageUrl,
+                userId,
             }
-
-            const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
-            const accountRepository = new InMemoryAccountRepository([walletAccountData])
-            const categoryRepository = new InMemoryCategoryRepository([categoryData])
-            const transactionRepository = new InMemoryTransactionRepository([])
-            const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
-            
-            const sut = new AddManualTransaction(addManualTransactionToWallet)
-            const response = (await sut.perform(transactionRequest)).value as Error
-            expect(response).toBeInstanceOf(InvalidTransactionError)
-
+            bankAccountData = {
+                id: bankAccountId,
+                type: bankAccountType,
+                syncType,
+                name: 'valid wallet account',
+                balance,
+                imageUrl,
+                userId,
+            }
         })
 
-        test('should add a valid transaction and update account balance', async () => {
+        test('should not add transaction if account is not found', async () => {
             const transactionRequest: TransactionRequest = {
-                accountId,
+                accountId: 'invalid account id',
                 categoryId,
                 amount,
                 description,
@@ -78,16 +71,218 @@ describe('Add manual transaction to account use case', () => {
             }
 
             const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
-            const accountRepository = new InMemoryAccountRepository([walletAccountData])
+            const accountRepository = new InMemoryAccountRepository([walletAccountData, bankAccountData])
             const categoryRepository = new InMemoryCategoryRepository([categoryData])
             const transactionRepository = new InMemoryTransactionRepository([])
             const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+            const addManualTransactionToBank = new AddManualTransactionToBank(userRepository, accountRepository, transactionRepository, categoryRepository)
             
-            const sut = new AddManualTransaction(addManualTransactionToWallet)
-            const response = (await sut.perform(transactionRequest)).value as TransactionData
-            expect(response.id).not.toBeUndefined()
-            expect(await transactionRepository.exists(response.id)).toBe(true)
-            expect((await accountRepository.findById(accountId)).balance).toBe(balance + transactionRequest.amount)
+            const sut = new AddManualTransaction(userRepository, accountRepository, categoryRepository, addManualTransactionToWallet, addManualTransactionToBank)
+            const response = (await sut.perform(transactionRequest)).value as Error
+            expect(response).toBeInstanceOf(UnregisteredAccountError)
+
+        })
+
+        test('should not add transaction if user of account is not found', async () => {
+            const transactionRequest: TransactionRequest = {
+                accountId: walletAccountId,
+                categoryId,
+                amount,
+                description,
+                date,
+                comment,
+                ignored,
+            }
+
+            const userRepository = new InMemoryUserRepository([])
+            const accountRepository = new InMemoryAccountRepository([walletAccountData, bankAccountData])
+            const categoryRepository = new InMemoryCategoryRepository([categoryData])
+            const transactionRepository = new InMemoryTransactionRepository([])
+            const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+            const addManualTransactionToBank = new AddManualTransactionToBank(userRepository, accountRepository, transactionRepository, categoryRepository)
+            
+            const sut = new AddManualTransaction(userRepository, accountRepository, categoryRepository, addManualTransactionToWallet, addManualTransactionToBank)
+            const response = (await sut.perform(transactionRequest)).value as Error
+            expect(response).toBeInstanceOf(UnregisteredUserError)
+
+        })
+
+        test('should not add transaction if category is not found', async () => {
+
+            const transactionRequest: TransactionRequest = {
+                accountId: walletAccountId,
+                categoryId: 'invalid',
+                amount,
+                date,
+                comment,
+                ignored,
+            }
+    
+            const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
+            const accountRepository = new InMemoryAccountRepository([walletAccountData, bankAccountData])
+            const categoryRepository = new InMemoryCategoryRepository([categoryData])
+            const transactionRepository = new InMemoryTransactionRepository([])
+            const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+            const addManualTransactionToBank = new AddManualTransactionToBank(userRepository, accountRepository, transactionRepository, categoryRepository)
+            
+            const sut = new AddManualTransaction(userRepository, accountRepository, categoryRepository,addManualTransactionToWallet, addManualTransactionToBank)
+            const response = (await sut.perform(transactionRequest)).value as Error
+            expect(response).toBeInstanceOf(UnregisteredCategoryError)
         })
     })
+
+    // describe.skip('add manual transaction to wallet account', () => {
+    //     const userId = 'u0'
+    //     const categoryId = 'c0'
+    //     const amount = -5060
+    //     const description = 'valid description'
+    //     const date = new Date('2023-03-09')
+    //     const comment = 'valid comment'
+    //     const ignored = false
+    
+    //     const categoryData: CategoryData = {
+    //         name: "category 0",
+    //         group: "group 0",
+    //         iconName: "icon 0",
+    //         primaryColor: "color 0",
+    //         ignored: true,
+    //         id: categoryId,
+    //     }
+        
+        
+    //     const accountId = 'ac0'
+    //     const accountType = 'WALLET'
+    //     const syncType = 'MANUAL'
+    //     const name = 'valid wallet account'
+    //     const balance = 678
+    //     const imageUrl = 'valid image url'
+    
+    //     let walletAccountData: WalletAccountData
+    
+    //     beforeEach(() => {
+    //         walletAccountData = {
+    //             id: accountId,
+    //             type: accountType,
+    //             syncType,
+    //             name,
+    //             balance,
+    //             imageUrl,
+    //             userId,
+    //         }
+    //     })
+
+    //     test('should not add transaction without required field', async () => {
+    //         const transactionRequest: TransactionRequest = {
+    //             accountId,
+    //             categoryId,
+    //             amount,
+    //             date,
+    //             comment,
+    //             ignored,
+    //         }
+
+    //         const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
+    //         const accountRepository = new InMemoryAccountRepository([walletAccountData])
+    //         const categoryRepository = new InMemoryCategoryRepository([categoryData])
+    //         const transactionRepository = new InMemoryTransactionRepository([])
+    //         const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+    //         const addManualTransactionToBank = new AddManualTransactionToBank(userRepository, accountRepository, transactionRepository, categoryRepository)
+            
+    //         const sut = new AddManualTransaction(addManualTransactionToWallet, addManualTransactionToBank)
+    //         const response = (await sut.perform(transactionRequest)).value as Error
+    //         expect(response).toBeInstanceOf(InvalidTransactionError)
+
+    //     })
+
+    //     test('should add a valid transaction and update account balance', async () => {
+    //         const transactionRequest: TransactionRequest = {
+    //             accountId,
+    //             categoryId,
+    //             amount,
+    //             description,
+    //             date,
+    //             comment,
+    //             ignored,
+    //         }
+
+    //         const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
+    //         const accountRepository = new InMemoryAccountRepository([walletAccountData])
+    //         const categoryRepository = new InMemoryCategoryRepository([categoryData])
+    //         const transactionRepository = new InMemoryTransactionRepository([])
+    //         const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+    //         const addManualTransactionToBank = new AddManualTransactionToBank(userRepository, accountRepository, transactionRepository, categoryRepository)
+            
+    //         const sut = new AddManualTransaction(addManualTransactionToWallet, addManualTransactionToBank)
+    //         const response = (await sut.perform(transactionRequest)).value as TransactionData
+    //         expect(response.id).not.toBeUndefined()
+    //         expect(await transactionRepository.exists(response.id)).toBe(true)
+    //         expect((await accountRepository.findById(accountId)).balance).toBe(balance + transactionRequest.amount)
+    //     })
+    // })
+
+    // describe.skip('add manual transaction to bank account', () => {
+    //     const userId = 'u0'
+    //     const categoryId = 'c0'
+    //     const amount = -5060
+    //     const description = 'valid description'
+    //     const date = new Date('2023-03-09')
+    //     const comment = 'valid comment'
+    //     const ignored = false
+    
+    //     const categoryData: CategoryData = {
+    //         name: "category 0",
+    //         group: "group 0",
+    //         iconName: "icon 0",
+    //         primaryColor: "color 0",
+    //         ignored: true,
+    //         id: categoryId,
+    //     }
+        
+        
+    //     const accountId = 'ac0'
+    //     const accountType = 'BANK'
+    //     const syncType = 'MANUAL'
+    //     const name = 'valid bank account'
+    //     const balance = 678
+    //     const imageUrl = 'valid image url'
+    
+    //     let bankAccountData: BankAccountData
+    
+    //     beforeEach(() => {
+    //         bankAccountData = {
+    //             id: accountId,
+    //             type: accountType,
+    //             syncType,
+    //             name,
+    //             balance,
+    //             imageUrl,
+    //             userId,
+    //         }
+    //     })
+
+    //     test('should not add transaction without required field', async () => {
+    //         const transactionRequest: TransactionRequest = {
+    //             accountId,
+    //             categoryId,
+    //             amount,
+    //             date,
+    //             comment,
+    //             ignored,
+    //         }
+
+    //         const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
+    //         const accountRepository = new InMemoryAccountRepository([bankAccountData])
+    //         const categoryRepository = new InMemoryCategoryRepository([categoryData])
+    //         const transactionRepository = new InMemoryTransactionRepository([])
+    //         const addManualTransactionToWallet = new AddManualTransactionToWallet(userRepository, accountRepository, transactionRepository, categoryRepository)
+    //         const addManualTransactionToBank = new AddManualTransactionToBank(userRepository, accountRepository, transactionRepository, categoryRepository)
+            
+    //         const sut = new AddManualTransaction(addManualTransactionToWallet, addManualTransactionToBank)
+    //         const response = (await sut.perform(transactionRequest)).value as Error
+    //         expect(response).toBeInstanceOf(InvalidTransactionError)
+
+    //     })
+    // })
+
+    
 })
