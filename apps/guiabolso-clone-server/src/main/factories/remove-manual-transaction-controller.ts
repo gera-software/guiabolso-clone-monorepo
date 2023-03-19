050ac6166f234a6ebb5f86bc29fb2b23987902a8
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-19 17:23:01 -0300

    fix [server]: makeRemoveManualTransactionController

--- apps/guiabolso-clone-server/src/main/factories/remove-manual-transaction-controller.ts
+++ apps/guiabolso-clone-server/src/main/factories/remove-manual-transaction-controller.ts
@@ -3,14 +3,16 @@ import { RemoveManualTransactionFromWallet } from "@/usecases/remove-manual-tran
 import { RemoveManualTransactionController } from "@/web-controllers";
 import { Controller } from "@/web-controllers/ports";
 import { makeAccountRepository, makeTransactionRepository, makeUserRepository } from "@/main/factories";
+import { RemoveManualTransactionFromBank } from "@/usecases/remove-manual-transaction-from-bank";
 
 export const makeRemoveManualTransactionController = (): Controller => {
     const userRepository = makeUserRepository()
     const accountRepository = makeAccountRepository()
     const transactionRepository = makeTransactionRepository()
     const removeManualTransactionFromWallet = new RemoveManualTransactionFromWallet(transactionRepository, accountRepository, userRepository)
+    const removeManualTransactionFromBank = new RemoveManualTransactionFromBank(transactionRepository, accountRepository, userRepository)
     
-    const usecase = new RemoveManualTransaction(removeManualTransactionFromWallet)
+    const usecase = new RemoveManualTransaction(transactionRepository, removeManualTransactionFromWallet, removeManualTransactionFromBank)
 
     const controller = new RemoveManualTransactionController(usecase)
     return controller
