9b6bb7b642672e681ffa371f81fee96d5a85fa08
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-19 10:33:07 -0300

    feat [server]: BankTransaction entity

---
+++ apps/guiabolso-clone-server/src/entities/bank-transaction.ts
@@ -0,0 +1,51 @@
+import { InvalidAmountError, InvalidTransactionError } from "@/entities/errors"
+import { Either, left, right } from "@/shared"
+import { Amount, Category, Transaction, TransactionType } from "@/entities"
+
+export class BankTransaction implements Transaction {
+    public readonly category: Category
+    public readonly amount: Amount
+    public readonly description: string
+    public readonly date: Date
+    public readonly comment: string
+    public readonly ignored: boolean
+    public readonly type: TransactionType
+
+    private constructor(transaction: { category: Category, amount: Amount, description?: string, date: Date, comment?: string, ignored?: boolean }) {
+        this.amount = transaction.amount
+        this.category = transaction.category ?? null
+        this.description = transaction.description ?? ''
+        this.date = transaction.date
+        this.comment = transaction.comment ?? ''
+        this.ignored = !!transaction.ignored
+        this.type = (this.amount.value >= 0) ? 'INCOME' : 'EXPENSE'
+    }
+
+    public static create(transaction: { category?: Category, amount: number, description?: string, date: Date, comment?: string, ignored?: boolean }): Either<InvalidTransactionError | InvalidAmountError, BankTransaction> {
+
+        if(!transaction.description) {
+            return left(new InvalidTransactionError('Required some description'))
+        }
+
+        if(transaction.amount === 0) {
+            return left(new InvalidTransactionError('Invalid amount'))
+        }
+
+        const amountOrError = Amount.create(transaction.amount)
+
+        if(amountOrError.isLeft()) {
+            return left(amountOrError.value)
+        }
+
+        const amount = amountOrError.value as Amount
+
+        return right(new BankTransaction({ 
+            amount, 
+            category: transaction.category,
+            description: transaction.description,
+            date: transaction.date,
+            comment: transaction.comment,
+            ignored: transaction.ignored,
+        }))
+    }
+}
---
+++ apps/guiabolso-clone-server/test/entities/bank-transaction.spec.ts
@@ -0,0 +1,84 @@
+import { Category, BankTransaction, TransactionType } from "@/entities"
+import { InvalidTransactionError } from "@/entities/errors"
+
+describe("Bank Transaction entity", () => {
+
+    const category = Category.create({
+        name: 'valid name',
+        group: 'GROUPNAME',
+        iconName: 'icon name',
+        primaryColor: 'color',
+        ignored: false,
+    }).value as Category
+
+    test("should not create transaction without description", () => {
+        const amount = -1900
+        const date = new Date('2023-01-23')
+        const error = BankTransaction.create({ amount, category, date }).value as Error
+        expect(error).toBeInstanceOf(InvalidTransactionError)
+        expect(error.message).toBe('Required some description')
+    })
+
+    test("should not create transaction with zero amount", () => {
+        const amount = 0
+        const description = 'transaction description'
+        const date = new Date('2023-01-23')
+        const error = BankTransaction.create({ amount, description, date }).value as Error
+        expect(error).toBeInstanceOf(InvalidTransactionError)
+        expect(error.message).toBe('Invalid amount')
+    })
+
+    test("should create transaction type expense", () => {
+        const amount = -1900
+        const description = 'transaction description'
+        const date = new Date('2023-01-23')
+        const type: TransactionType = 'EXPENSE'
+        const comment = 'comentario válido'
+        const ignored = true
+        const transaction = BankTransaction.create({ amount, description, date, comment, ignored }).value as BankTransaction
+        expect(transaction.category).toEqual(null)
+        expect(transaction.amount.value).toEqual(amount)
+        expect(transaction.description).toEqual(description)
+        // expect(transaction.descriptionOriginal).toEqual('')
+        expect(transaction.date.toISOString()).toEqual('2023-01-23T00:00:00.000Z')
+        expect(transaction.type).toEqual(type)
+        expect(transaction.comment).toEqual(comment)
+        expect(transaction.ignored).toEqual(ignored)
+    })
+
+    test("should create transaction type income", () => {
+        const amount = 1900
+        const description = 'transaction description'
+        const date = new Date('2023-01-23')
+        const type: TransactionType = 'INCOME'
+        const comment = 'comentario válido'
+        const ignored = true
+        const transaction = BankTransaction.create({ amount, description, date, comment, ignored }).value as BankTransaction
+        expect(transaction.category).toEqual(null)
+        expect(transaction.amount.value).toEqual(amount)
+        expect(transaction.description).toEqual(description)
+        // expect(transaction.descriptionOriginal).toEqual('')
+        expect(transaction.date.toISOString()).toEqual('2023-01-23T00:00:00.000Z')
+        expect(transaction.type).toEqual(type)
+        expect(transaction.comment).toEqual(comment)
+        expect(transaction.ignored).toEqual(ignored)
+    })
+
+    test("should create transaction with optional category", () => {
+        const amount = -1900
+        const description = 'transaction description'
+        const date = new Date('2023-01-23')
+        const type: TransactionType = 'EXPENSE'
+        const transaction = BankTransaction.create({ amount, category, description, date }).value as BankTransaction
+        expect(transaction.category).toEqual(category)
+        expect(transaction.amount.value).toEqual(amount)
+        expect(transaction.description).toEqual(description)
+        // expect(transaction.descriptionOriginal).toEqual('')
+        expect(transaction.type).toEqual(type)
+        expect(transaction.comment).toEqual('')
+        expect(transaction.ignored).toEqual(false)
+    })
+
+
+
+})
