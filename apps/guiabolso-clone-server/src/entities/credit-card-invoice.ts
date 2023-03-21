1c4ba8bcca45b9925c76dcb396d747270d0b7fbf
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-03-21 13:46:05 -0300

    feat [server]: Credit Card Invoice entity date validations

--- apps/guiabolso-clone-server/src/entities/credit-card-invoice.ts
+++ apps/guiabolso-clone-server/src/entities/credit-card-invoice.ts
@@ -1,5 +1,6 @@
-import { right } from "@/shared";
+import { left, right } from "@/shared";
 import { Amount, CreditCardAccount } from "@/entities";
+import { InvalidCreditCardInvoiceError } from "./errors";
 
 export class CreditCardInvoice {
     public readonly closeDate: Date
@@ -15,6 +16,16 @@ export class CreditCardInvoice {
     }
 
     public static create(invoice: { closeDate: Date, dueDate: Date, amount: number, account: CreditCardAccount }) {
+        if(invoice.dueDate <= invoice.closeDate) {
+            return left(new InvalidCreditCardInvoiceError('Close date must be before due date'))
+        }
+
+        const diffInMs = invoice.dueDate.getTime() - invoice.closeDate.getTime()
+        const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+        if(diffInDays > 10) {
+            return left(new InvalidCreditCardInvoiceError('The diference between dueDate and close date should be smaller than 10 days'))
+        }
+        
         const amount = Amount.create(invoice.amount).value as Amount
 
         return right(new CreditCardInvoice({
--- apps/guiabolso-clone-server/test/entities/credit-card-invoice.spec.ts
+++ apps/guiabolso-clone-server/test/entities/credit-card-invoice.spec.ts
@@ -1,4 +1,5 @@
 import { CreditCardAccount, CreditCardInvoice, Institution, User } from "@/entities"
+import { InvalidCreditCardInvoiceError } from "@/entities/errors"
 
 describe("Credit Card Invoice entity", () => {
     const accountName = 'valid name'
@@ -26,22 +27,69 @@ describe("Credit Card Invoice entity", () => {
         providerConnectorId: 'valid id'
     }).value as Institution
 
-    const closeDate = new Date('2023-02-10')
-    const dueDate = new Date('2023-02-03')
+    const closeDate = new Date('2023-02-03')
+    const dueDate = new Date('2023-02-10')
     const amount = 6000
 
-    test("should create an invoice with valid fields", () => {
-        const account: CreditCardAccount = CreditCardAccount.create({name: accountName, balance: accountBalance, imageUrl, user, institution, creditCardInfo}).value as CreditCardAccount
-        const validInvoice = {
-            closeDate,
-            dueDate,
-            amount,
-            account,
-        }
-        const response = CreditCardInvoice.create(validInvoice).value as CreditCardInvoice
-        expect(response.closeDate.toISOString()).toEqual(closeDate.toISOString())
-        expect(response.dueDate.toISOString()).toEqual(dueDate.toISOString())
-        expect(response.amount.value).toEqual(amount)
-        expect(response.account.name).toEqual(account.name)
-    }) 
+    describe('create', () => {
+    
+        test("should not create an invoice if dueDate is before close date", () => {
+            const account: CreditCardAccount = CreditCardAccount.create({name: accountName, balance: accountBalance, imageUrl, user, institution, creditCardInfo}).value as CreditCardAccount
+            const validInvoice = {
+                closeDate: new Date('2023-03-10'),
+                dueDate: new Date('2023-02-28'),
+                amount,
+                account,
+            }
+            const response = CreditCardInvoice.create(validInvoice).value as Error
+            expect(response).toBeInstanceOf(InvalidCreditCardInvoiceError)
+        }) 
+    
+        test("should not create an invoice if diference between due date and close date is bigger than 10 days", () => {
+            const account: CreditCardAccount = CreditCardAccount.create({name: accountName, balance: accountBalance, imageUrl, user, institution, creditCardInfo}).value as CreditCardAccount
+            const validInvoice = {
+                closeDate: new Date('2023-10-21'),
+                dueDate: new Date('2023-11-01'),
+                amount,
+                account,
+            }
+            const response = CreditCardInvoice.create(validInvoice).value as Error
+            expect(response).toBeInstanceOf(InvalidCreditCardInvoiceError)
+        }) 
+    
+        test("should create an invoice if diference between dueDate and close date is less or equal than 10 days", () => {
+            const account: CreditCardAccount = CreditCardAccount.create({name: accountName, balance: accountBalance, imageUrl, user, institution, creditCardInfo}).value as CreditCardAccount
+            const validInvoice = {
+                closeDate: new Date('2023-10-22'),
+                dueDate: new Date('2023-11-01'),
+                amount,
+                account,
+            }
+            const response = CreditCardInvoice.create(validInvoice).value as CreditCardInvoice
+            expect(response.closeDate).toEqual(new Date('2023-10-22'))
+            expect(response.dueDate).toEqual(new Date('2023-11-01'))
+            expect(response.amount.value).toEqual(amount)
+            expect(response.account.name).toEqual(account.name)
+        }) 
+    
+        test("should create an invoice with valid fields", () => {
+            const account: CreditCardAccount = CreditCardAccount.create({name: accountName, balance: accountBalance, imageUrl, user, institution, creditCardInfo}).value as CreditCardAccount
+            const validInvoice = {
+                closeDate: new Date(closeDate),
+                dueDate: new Date(dueDate),
+                amount,
+                account,
+            }
+            const response = CreditCardInvoice.create(validInvoice).value as CreditCardInvoice
+            expect(response.closeDate).toEqual(new Date(closeDate))
+            expect(response.dueDate).toEqual(new Date(dueDate))
+            expect(response.amount.value).toEqual(amount)
+            expect(response.account.name).toEqual(account.name)
+        }) 
+    })
+
+    test.todo('should not add a transaction witch date is out of invoice date range (before date, exact date, after date)')
+    test.todo('should not add a transaction type "pagamento de cartão" to invoice balance')
+    test.todo('should add a transaction (expense, income) and update invoice balance')
+    test.todo('should remove a transaction (expense, income) and update invoice balance')
 })
