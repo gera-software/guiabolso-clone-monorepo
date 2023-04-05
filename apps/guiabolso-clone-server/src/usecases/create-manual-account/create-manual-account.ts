decfac0774211ecf633d3f7176db5d8b227e1d3a
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-04-04 21:43:31 -0300

    feat [server]:  create manual account use case should create a bank account

--- apps/guiabolso-clone-server/src/usecases/create-manual-account/create-manual-account.ts
+++ apps/guiabolso-clone-server/src/usecases/create-manual-account/create-manual-account.ts
@@ -2,13 +2,20 @@ import { AccountData, UseCase } from "@/usecases/ports";
 
 export class CreateManualAccount implements UseCase {
     private readonly createManualWalletAccount: UseCase
+    private readonly createManualBankAccount: UseCase
 
-    constructor(createManualWalletAccount: UseCase) {
+    constructor(createManualWalletAccount: UseCase, createManualBankAccount: UseCase) {
         this.createManualWalletAccount = createManualWalletAccount
+        this.createManualBankAccount = createManualBankAccount
     }
 
     async perform(request: AccountData): Promise<any> {
-        return this.createManualWalletAccount.perform(request)
+        switch(request.type) {
+            case 'WALLET':
+                return this.createManualWalletAccount.perform(request)
+            case 'BANK':
+                return this.createManualBankAccount.perform(request)
+        }
     }
 
 }
--- apps/guiabolso-clone-server/test/usecases/create-manual-account.spec.ts
+++ apps/guiabolso-clone-server/test/usecases/create-manual-account.spec.ts
@@ -1,7 +1,8 @@
 import { CreateManualAccount } from "@/usecases/create-manual-account"
+import { CreateManualBankAccount } from "@/usecases/create-manual-bank-account"
 import { CreateManualWalletAccount } from "@/usecases/create-manual-wallet-account"
-import { AccountData, WalletAccountData } from "@/usecases/ports"
-import { InMemoryAccountRepository, InMemoryUserRepository } from "@test/doubles/repositories"
+import { AccountData, BankAccountData, InstitutionData, WalletAccountData } from "@/usecases/ports"
+import { InMemoryAccountRepository, InMemoryInstitutionRepository, InMemoryUserRepository } from "@test/doubles/repositories"
 
 describe('Create manual account use case', () => {
 
@@ -25,14 +26,63 @@ describe('Create manual account use case', () => {
     
             const accountRepository = new InMemoryAccountRepository([])
             const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
+            const institutionRepository = new InMemoryInstitutionRepository([])
             const createManualWalletAccount = new CreateManualWalletAccount(accountRepository, userRepository)
-            const sut = new CreateManualAccount(createManualWalletAccount)
+            const createManualBankAccount = new CreateManualBankAccount(accountRepository, userRepository, institutionRepository)
+            const sut = new CreateManualAccount(createManualWalletAccount, createManualBankAccount)
 
             const response = (await sut.perform(createManualWalletRequest)).value as AccountData
     
             const addedAccount = await accountRepository.exists(response.id)
             expect(addedAccount).toBeTruthy()
+            expect(response.type).toBe(type)
             expect(response.id).toBeTruthy()
         })
     })
+
+    describe('manual bank account', () => {
+        test('should create manual bank account if all params are valid', async () => {
+            const type = 'BANK'
+            const syncType = 'MANUAL'
+            const name = 'valid account'
+            const balance = 670
+            const imageUrl = 'valid image url'
+            const userId = 'valid user id'
+            const institution: InstitutionData = {
+                id: 'id 0',
+                name: 'institution name',
+                type: 'PERSONAL_BANK',
+                imageUrl: 'url',
+                primaryColor: 'color',
+                providerConnectorId: 'valid id'
+            }
+    
+            const createManualBankRequest: BankAccountData = {
+                type,
+                syncType,
+                name,
+                balance,
+                userId,
+                imageUrl,
+                institution,
+            }
+    
+            const accountRepository = new InMemoryAccountRepository([])
+            const userRepository = new InMemoryUserRepository([{ id: userId, name: 'any name', email: 'any@email.com', password: '123' }])
+            const institutionRepository = new InMemoryInstitutionRepository([institution])
+            const createManualWalletAccount = new CreateManualWalletAccount(accountRepository, userRepository)
+            const createManualBankAccount = new CreateManualBankAccount(accountRepository, userRepository, institutionRepository)
+            
+            const sut = new CreateManualAccount(createManualWalletAccount, createManualBankAccount)
+
+            const response = (await sut.perform(createManualBankRequest)).value as AccountData
+    
+            const addedAccount = await accountRepository.exists(response.id)
+            expect(addedAccount).toBeTruthy()
+            expect(response.id).toBeTruthy()
+            expect(response.type).toBe(type)
+            expect(response.institution).toEqual(institution)
+        })
+    })
+    
 })
