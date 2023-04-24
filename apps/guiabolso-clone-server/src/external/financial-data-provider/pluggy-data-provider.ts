d19388e2a022ffe8d740ac629c76ff2f4e769b9f
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-04-24 18:29:43 -0300

    feat [server]: Pluggy Data Provider - getAccountsByItemId should return a list of accounts

--- apps/guiabolso-clone-server/src/external/financial-data-provider/pluggy-data-provider.ts
+++ apps/guiabolso-clone-server/src/external/financial-data-provider/pluggy-data-provider.ts
@@ -39,7 +39,49 @@ export class PluggyDataProvider implements FinancialDataProvider {
         }
     }
 
-    public async getAccountsByItemId(itemId: string): Promise<Either<UnexpectedError, AccountData[]>> {
-        throw new Error('Method getAccountsByItemId not implemented.')
+    public async getAccountsByItemId(itemId: string): Promise<Either<UnexpectedError, AccountData[]>> { 
+        try {
+            const item = await this.client.fetchItem(itemId)
+
+            const institutionData: InstitutionData = {
+                id: null,
+                name: item.connector.name,
+                type: item.connector.type,
+                imageUrl: item.connector.imageUrl,
+                primaryColor: item.connector.primaryColor,
+                providerConnectorId: ''+item.connector.id,
+            }
+
+            const accountsArray = await this.client.fetchAccounts(itemId)
+
+            const results = accountsArray.results.map(account => ({
+                id: null,
+                type: account.type == 'BANK' ? 'BANK' : 'CREDIT_CARD',
+                syncType: 'AUTOMATIC',
+                name: account.name,
+                balance: account.type == 'BANK' ? account.balance :  -account.balance,
+                imageUrl: item.connector.imageUrl,
+                userId: null,
+                institution: institutionData,
+                creditCardInfo: account.type == 'BANK' ? null : {
+                    brand: account.creditData.brand,
+                    creditLimit: account.creditData.creditLimit,
+                    availableCreditLimit: account.creditData.availableCreditLimit,
+                    closeDay: account.creditData.balanceCloseDate.getUTCDate(),
+                    dueDay: account.creditData.balanceDueDate.getUTCDate(),
+                },
+                providerAccountId: account.id,
+                synchonization: {
+                    providerItemId: item.id,
+                    createdAt: item.createdAt,
+                }
+            }))
+
+    
+            return right(results)
+        } catch (error: any) {
+            return left(new UnexpectedError(error.message))
+        }
+
     }
 }
--- apps/guiabolso-clone-server/test/external/financial-data-provider/pluggy-data-provider.spec.ts
+++ apps/guiabolso-clone-server/test/external/financial-data-provider/pluggy-data-provider.spec.ts
@@ -1,6 +1,7 @@
-import { Connector, PluggyClient } from 'pluggy-sdk'
+import { Connector, Item, ItemStatus, Account as PluggyAccount, PluggyClient } from 'pluggy-sdk'
 import { PluggyDataProvider } from "@/external/financial-data-provider"
 import { UnexpectedError } from '@/usecases/errors'
+import { AccountData } from '@/usecases/ports'
 jest.mock('pluggy-sdk')
 const mockedPluggyClient = jest.mocked(PluggyClient)
 
@@ -110,6 +111,227 @@ describe('Pluggy Data Provider', () => {
             const itemId = 'valid-item-id'
             const result = (await sut.getConnectToken(itemId)).value as Error
             expect(result).toBeInstanceOf(UnexpectedError)
+        })
+    })
+
+    describe('getAccountsByItemId', () => {
+        test('should return a error if item not found', async () => {
+            mockedPluggyClient.prototype.fetchItem.mockRejectedValueOnce({ code: 404, message: "item not found" })
+            const validClientId = 'valid-client-id'
+            const validClientSecret = 'valid-client-secret'
+            const sut = new PluggyDataProvider(validClientId, validClientSecret)
+
+            const itemId = 'invalid-item-id'
+            const result = (await sut.getAccountsByItemId(itemId)).value as Error
+            expect(result).toBeInstanceOf(UnexpectedError)
+        })
+
+        test('should return a error if provider api has an internal error', async () => {
+            mockedPluggyClient.prototype.fetchItem.mockRejectedValueOnce({ code: 500, message: "Internal Server Error" })
+            const validClientId = 'valid-client-id'
+            const validClientSecret = 'valid-client-secret'
+            const sut = new PluggyDataProvider(validClientId, validClientSecret)
+
+            const itemId = 'valid-item-id'
+            const result = (await sut.getAccountsByItemId(itemId)).value as Error
+            expect(result).toBeInstanceOf(UnexpectedError)
+        })
+
+        test('should return a list of accounts', async () => {
+            const validItemId = "a5d1ca6c-24c0-41c7-8b44-9272cc868663"
+
+            const item: Item = {
+                "id": validItemId,
+                "createdAt": new Date("2021-12-28T21:48:02.863Z"),
+                // "updatedAt": new Date("2021-12-28T21:48:02.952Z"),
+                "connector": {
+                    "id": 201,
+                    "name": "Itaú",
+                    "primaryColor": "EC7000",
+                    "institutionUrl": "https://www.itau.com.br",
+                    "country": "BR",
+                    "type": "PERSONAL_BANK",
+                    "credentials": [
+                    {
+                        "label": "Agência",
+                        "name": "agency",
+                        "type": "number",
+                        "placeholder": "Agência",
+                        "validation": "^\\d{4}$",
+                        "validationMessage": "O agencia deve ter 4 dígito"
+                    },
+                    {
+                        "label": "Conta",
+                        "name": "account",
+                        "type": "number",
+                        "placeholder": "Conta",
+                        "validation": "^\\d{4,6}$",
+                        "validationMessage": "O conta deve ter 6 dígito"
+                    },
+                    {
+                        "label": "Senha",
+                        "name": "password",
+                        "type": "number",
+                        "placeholder": "Senha",
+                        "validation": "^\\d{6}$",
+                        "validationMessage": "O senha deve ter 6 dígito"
+                    }
+                    ],
+                    "imageUrl": "https://res.cloudinary.com/dkr0vihmp/image/upload/v1588853552/connectors-logos/itau_ntodvn.png",
+                    "hasMFA": false,
+                },
+                "status": ItemStatus.UPDATING,
+                "executionStatus": "CREATED",
+                "lastUpdatedAt": null,
+                "webhookUrl": null,
+                // "error": null,
+                "clientUserId": "My User App Id",
+                "parameter": null,
+                // "userAction": null,
+                // "statusDetail": {
+                //   "accounts": {
+                //     "isUpdated": true,
+                //     "lastUpdatedAt": "2022-03-08T22:43:04.796Z",
+                //     "warnings": []
+                //   },
+                //   "identity": {
+                //     "isUpdated": false,
+                //     "lastUpdatedAt": null,
+                //     "warnings": []
+                //   },
+                //   "creditCards": {
+                //     "isUpdated": true,
+                //     "lastUpdatedAt": "2022-03-08T22:43:04.796Z",
+                //     "warnings": []
+                //   },
+                //   "investments": {
+                //     "isUpdated": true,
+                //     "lastUpdatedAt": "2022-03-08T22:43:04.796Z",
+                //     "warnings": [
+                //       {
+                //         "code": "001",
+                //         "message": "You lack permissions to view Investments on this account",
+                //         "providerMessage": "Meu perfil não permite visualizar investimentos"
+                //       }
+                //     ]
+                //   },
+                //   "transactions": {
+                //     "isUpdated": true,
+                //     "lastUpdatedAt": "2022-03-08T22:43:04.796Z",
+                //     "warnings": []
+                //   },
+                //   "paymentData": null
+                // },
+                // "nextAutoSyncAt": null
+            }
+
+            const bankAccount: PluggyAccount = {
+                "id": "a658c848-e475-457b-8565-d1fffba127c4",
+                "type": "BANK",
+                "subtype": "CHECKINGS_ACCOUNT",
+                "number": "0001/12345-0",
+                "name": "Conta Corrente",
+                "marketingName": "GOLD Conta Corrente",
+                "balance": 120950,
+                "itemId": validItemId,
+                "taxNumber": "416.799.495-00",
+                "owner": "John Doe",
+                "currencyCode": "BRL",
+                "bankData": {
+                  "transferNumber": "0001/12345-0",
+                  "closingBalance": 120950
+                }
+            }
+
+            const creditAccount: PluggyAccount = {
+                "id": "a658c848-e475-457b-8565-d1fffba127c4",
+                "type": "CREDIT",
+                "subtype": "CREDIT_CARD",
+                "number": "xxxx8670",
+                "name": "Mastercard Black",
+                "marketingName": "PLUGGY UNICLASS MASTERCARD BLACK",
+                "balance": 120950,
+                "itemId": validItemId,
+                "taxNumber": "416.799.495-00",
+                "owner": "John Doe",
+                "currencyCode": "BRL",
+                "creditData": {
+                  "level": "BLACK",
+                  "brand": "MASTERCARD",
+                  "balanceCloseDate": new Date("2022-01-03"),
+                  "balanceDueDate": new Date("2022-01-10"),
+                  "availableCreditLimit": 200000,
+                  "balanceForeignCurrency": 0,
+                  "minimumPayment": 16190,
+                  "creditLimit": 300000
+                }
+            }
+            
+            mockedPluggyClient.prototype.fetchItem.mockResolvedValueOnce(item)
+
+            mockedPluggyClient.prototype.fetchAccounts.mockResolvedValueOnce({
+                "results": [ bankAccount, creditAccount ]
+            })
+
+            const validClientId = 'valid-client-id'
+            const validClientSecret = 'valid-client-secret'
+            const sut = new PluggyDataProvider(validClientId, validClientSecret)
+
+            const results = (await sut.getAccountsByItemId(validItemId)).value as AccountData[]
+            expect(results.length).toBe(2)
+            expect(results[0]).toEqual({
+                "balance": bankAccount.balance, 
+                "creditCardInfo": null, 
+                "id": null, 
+                "imageUrl": item.connector.imageUrl, 
+                "institution": {
+                    "id": null, 
+                    "imageUrl": item.connector.imageUrl, 
+                    "name": item.connector.name, 
+                    "primaryColor": item.connector.primaryColor, 
+                    "providerConnectorId": ''+item.connector.id, 
+                    "type": item.connector.type
+                }, 
+                "name": bankAccount.name, 
+                "providerAccountId": bankAccount.id, 
+                "syncType": "AUTOMATIC", 
+                "synchonization": {
+                    "createdAt": item.createdAt, 
+                    "providerItemId": item.id
+                }, 
+                "type": "BANK", 
+                "userId": null,
+            })
+
+            expect(results[1]).toEqual({
+                "balance": -creditAccount.balance, 
+                "creditCardInfo": {
+                    "availableCreditLimit": creditAccount.creditData.availableCreditLimit, 
+                    "creditLimit": 300000, 
+                    "brand": creditAccount.creditData.brand, 
+                    "closeDay": creditAccount.creditData.balanceCloseDate.getUTCDate(), 
+                    "dueDay": creditAccount.creditData.balanceDueDate.getUTCDate()
+                }, 
+                "id": null, 
+                "imageUrl": item.connector.imageUrl, 
+                "institution": {
+                    "id": null, 
+                    "imageUrl": item.connector.imageUrl, 
+                    "name": item.connector.name, 
+                    "primaryColor": item.connector.primaryColor, 
+                    "providerConnectorId": ''+item.connector.id, 
+                    "type": item.connector.type
+                },
+                "name": creditAccount.name, 
+                "providerAccountId": creditAccount.id, 
+                "syncType": "AUTOMATIC", 
+                "synchonization": {
+                    "createdAt": item.createdAt, 
+                    "providerItemId": item.id
+                }, 
+                "type": "CREDIT_CARD", 
+                "userId": null,
+            })
         })
     })
 })
