fe7065c58580f26d5ce69b6c44d0c99c4b87f226
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-05-24 19:04:36 -0300

    fix [pwa]:  accountId undefined

--- apps/guiabolso-clone-pwa/netlify/functions/credit-card-invoices-fetch-by-account.ts
+++ apps/guiabolso-clone-pwa/netlify/functions/credit-card-invoices-fetch-by-account.ts
@@ -3,8 +3,7 @@ import * as CreditCardInvoiceRepository from '../repositories/creditCardInvoiceR
 import { CreditCardInvoice } from "../types";
 
 const handler :Handler = async (event, context) => {
-    const accountId = event.queryStringParameters?.accountId
-
+    const accountId = event.queryStringParameters?.accountId ?? ''
     const invoices: CreditCardInvoice[] = await CreditCardInvoiceRepository.fetchByAccount(accountId)
     
     return {
